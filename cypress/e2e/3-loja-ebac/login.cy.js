9e415cf8600d25a1e296ba67ad94169d6318376c
William-Dantas-Dev <shiroe.gaming.dev@gmail.com>
2025-07-20 21:09:55 -0300

    automated test for invalid user and invalid password

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,14 +1,40 @@
 /// <reference types="cypress" />
 
 describe('Function: Login', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
     
     it('Login', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('wil@gmail.com')
         cy.get('#password').type('teste@123')
         cy.get('.woocommerce-form > .button').click()
 
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, wil (não é wil? Sair)')
     })
+
+    it('Show Error Invalid User', () => {
+        cy.get('#username').type('wil123@gmail.com')
+        cy.get('#password').type('teste@123')
+        cy.get('.woocommerce-form > .button').click()
+
+        // cy.get('.woocommerce-error > li').should('exist')
+        cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+    });
+
+    it('Show Error Invalid Password', () => {
+        cy.get('#username').type('wil@gmail.com')
+        cy.get('#password').type('123')
+        cy.get('.woocommerce-form > .button').click()
+
+        // cy.get('.woocommerce-error > li').should('exist')
+        cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida para o e-mail wil@gmail.com está incorreta. Perdeu a senha?')
+    });
 
 })
